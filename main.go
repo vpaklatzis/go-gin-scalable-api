e3c3f18ec27ee17f0dd3530cf7e5bf706dd9c164
vpaklatzis <vpaklatzis@gmail.com>
2022-05-23 02:18:31 +0300

    added get recipes endpoint

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/xid"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -33,13 +36,23 @@ func NewRecipeHandler(context *gin.Context) {
 	context.JSON(http.StatusOK, recipe)
 }
 
-// Initializes the recipes variable. Gets executed at the application startup.
+func ListRecipeHandler(context *gin.Context) {
+	fmt.Printf("ClientIP: %s\n", context.ClientIP())
+	context.JSON(http.StatusOK, recipes)
+}
+
+// Initializes the recipes variable. Reads the recipes.json file and converts the content
+// into an array of recipes. Gets executed at the application startup.
 func init() {
 	recipes = make([]Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipes)
 }
 
 func main() {
 	router := gin.Default()
+	router.SetTrustedProxies([]string{"localhost"})
 	router.POST("/recipes", NewRecipeHandler)
+	router.GET("/recipes", ListRecipeHandler)
 	router.Run()
 }
