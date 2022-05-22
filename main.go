5776bb53afed814ab1cf0cc5290d3f591d3ec14b
vpaklatzis <vpaklatzis@gmail.com>
2022-05-23 01:53:47 +0300

    added the post recipe endpoint

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
-package gin_scalable_api
+package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+	"net/http"
 	"time"
 )
 
 type Recipe struct {
+	ID           string    `json:"id"`
 	Name         string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
@@ -13,7 +16,30 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+// Global variable to temporarily store recipes. Will be replaced later.
+var recipes []Recipe
+
+func NewRecipeHandler(context *gin.Context) {
+	var recipe Recipe
+	if err := context.ShouldBindJSON(&recipe); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	context.JSON(http.StatusOK, recipe)
+}
+
+// Initializes the recipes variable. Gets executed at the application startup.
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
 func main() {
 	router := gin.Default()
+	router.POST("/recipes", NewRecipeHandler)
 	router.Run()
 }
